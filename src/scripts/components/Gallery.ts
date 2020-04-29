3bdfe2b7aac0b159cbbd9bcd22a1508f0bba5195
Radek Sukup <sukup.r@gmail.com>
2020-04-29 17:30:14 +0200

    Enable open lightbox by using `enter` key.

--- src/scripts/components/Gallery.ts
+++ src/scripts/components/Gallery.ts
@@ -3,7 +3,12 @@ export const Gallery = (container: HTMLElement): void => {
     // https://github.com/blueimp/Gallery/blob/master/README.md#setup
     container.onclick = (event: MouseEvent) => {
       event = event || window.event
-      const target = (event.target as HTMLImageElement) || event.srcElement
+      let target: HTMLImageElement | null = (event.target as HTMLImageElement) || event.srcElement
+
+      // enable open lightbox by using `enter` key
+      if (target.tagName.toLowerCase() === 'a') {
+        target = target.querySelector('img')
+      }
 
       // <img> must be direct descendant of <a> element
       // need adjustment in case of pseudo elements on <a>
@@ -18,6 +23,7 @@ export const Gallery = (container: HTMLElement): void => {
         // gallery initialization
         window.blueimp.Gallery(links, options)
       }
+
       return false
     }
   }
