f8e363970e267ee72cfec8984732531443a25020
Radek Sukup <radek.sukup@actum.cz>
2021-12-02 19:14:58 +0100

    Simplify navbar handlers + single variable file.

--- src/scripts/components/Navbar.ts
+++ src/scripts/components/Navbar.ts
@@ -1,42 +1,20 @@
 import { FocusTrap } from '../utils'
 
-const excludeLinks = (links: HTMLAnchorElement[]): void => {
-  links.forEach((link) => {
-    link.setAttribute('tabindex', '-1')
-  })
-}
-
-const includeLinks = (links: HTMLAnchorElement[]): void => {
-  links.forEach((link) => {
-    link.removeAttribute('tabindex')
-  })
-}
-
-const matchMediaHandler = (media: MediaQueryList): Promise<boolean> =>
-  new Promise((resolve) => {
-    if (media.matches) {
-      resolve(true)
-    } else {
-      resolve(false)
-    }
-  })
-
 export const Navbar = (navbar: HTMLElement): void => {
   const esc = {
     KEY: 'escape',
   }
   const classes = {
     IS_ACTIVE: 'is-active',
-    NAVBAR_VISIBLE: 'navbar-visible',
+    NAVBAR_OPEN: 'navbar-open',
     NAVBAR_BACKDROP: 'navbar-backdrop',
+    NAVBAR_CLOSING: 'navbar-closing',
   }
   const hamburger = document.querySelector('.js-hamburger')
   const body = document.body
   const pageOverlay = document.createElement('div')
   const showNavbarEvent = new CustomEvent('show.navbar')
   const hideNavbarEvent = new CustomEvent('hide.navbar')
-  const navbarLinks = Array.from(navbar.querySelectorAll('a'))
-  const navbarMobileBp = getComputedStyle(navbar).getPropertyValue('--navbar-mobile-bp')
   const focusTrap = new FocusTrap(navbar)
 
   pageOverlay.className = classes.NAVBAR_BACKDROP
@@ -46,7 +24,11 @@ export const Navbar = (navbar: HTMLElement): void => {
   body.appendChild(pageOverlay)
 
   hamburger?.addEventListener('click', () => {
-    if (!body.classList.contains(classes.NAVBAR_VISIBLE)) {
+    if (body.classList.contains(classes.NAVBAR_CLOSING)) {
+      return
+    }
+
+    if (!body.classList.contains(classes.NAVBAR_OPEN)) {
       navbar.dispatchEvent(showNavbarEvent)
     } else {
       navbar.dispatchEvent(hideNavbarEvent)
@@ -54,67 +36,35 @@ export const Navbar = (navbar: HTMLElement): void => {
   })
 
   navbar.addEventListener('show.navbar', () => {
-    body.classList.add(classes.NAVBAR_VISIBLE)
+    body.classList.add(classes.NAVBAR_OPEN)
     hamburger?.classList.add(classes.IS_ACTIVE)
     hamburger?.setAttribute('aria-expanded', 'true')
-
-    // include links back to tab flow
-    includeLinks(navbarLinks)
 
     focusTrap.activate()
   })
 
   navbar.addEventListener('hide.navbar', () => {
-    body.classList.remove(classes.NAVBAR_VISIBLE)
+    body.classList.add(classes.NAVBAR_CLOSING)
+    body.classList.remove(classes.NAVBAR_OPEN)
     hamburger?.classList.remove(classes.IS_ACTIVE)
     hamburger?.setAttribute('aria-expanded', 'false')
 
-    // exclude "hidden" links from tab flow
-    excludeLinks(navbarLinks)
-
     focusTrap.destroy()
+  })
+
+  navbar.addEventListener('transitionend', () => {
+    // navbar is hidden
+    if (!body.classList.contains(classes.NAVBAR_OPEN)) {
+      body.classList.remove(classes.NAVBAR_CLOSING)
+    }
   })
 
   // hide the navbar using ESC key
   document.addEventListener('keydown', (event: KeyboardEvent) => {
     if (event.key?.toLowerCase() === esc.KEY) {
-      if (body.classList.contains(classes.NAVBAR_VISIBLE)) {
+      if (body.classList.contains(classes.NAVBAR_OPEN)) {
         navbar.dispatchEvent(hideNavbarEvent)
       }
     }
   })
-
-  const matchMedia = !!navbarMobileBp && window.matchMedia(`(max-width: ${navbarMobileBp})`)
-
-  // if custom css property was provided => check for breakpoint changes
-  if (matchMedia) {
-    // run media check once when the page is loaded
-    matchMediaHandler(matchMedia).then((matched) => {
-      if (matched) {
-        excludeLinks(navbarLinks)
-      }
-    })
-
-    // check for breakpoint changes
-    // safari doesn't support addEventListener method
-    if (matchMedia.addEventListener !== undefined) {
-      matchMedia.addEventListener('change', () => {
-        matchMediaHandler(matchMedia).then((matched) => {
-          if (matched && !body.classList.contains(classes.NAVBAR_VISIBLE)) {
-            excludeLinks(navbarLinks)
-          } else {
-            includeLinks(navbarLinks)
-          }
-        })
-      })
-    } else {
-      matchMedia.addListener((event) => {
-        if (event.matches && !body.classList.contains(classes.NAVBAR_VISIBLE)) {
-          excludeLinks(navbarLinks)
-        } else {
-          includeLinks(navbarLinks)
-        }
-      })
-    }
-  }
 }
